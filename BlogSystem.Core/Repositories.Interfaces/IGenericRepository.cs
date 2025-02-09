a7f63c171e8559802a94f1508bef78cc4d17f624
amrabbas1 <amrabbas2003@gmail.com>
2025-02-10 01:45:37 +0200

    Add Generic Repository

---
+++ BlogSystem.Core/Repositories.Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Core.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(string id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
---
+++ BlogSystem.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using BlogSystem.Core.Repositories.Interfaces;
+using BlogSystem.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Repository.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly BlogDbContext _context;
+
+        public GenericRepository(BlogDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+        public async Task<TEntity> GetAsync(string id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            _context.Update(entity);
+        }
+        public void Delete(TEntity entity)
+        {
+            _context.Remove(entity);
+        }
+    }
+}
